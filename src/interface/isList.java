09523bc755a4996dc02bff32da8ac74dccea22ca
duyddawngnef <duyddang178@gmail.com>
2025-10-21 10:31:54 +0700

    thiết lập interface

---
+++ src/interface/isList.java
@@ -0,0 +1,19 @@
+package Interface;
+
+public interface isList {
+    //đọc file
+    void read(String filename);
+    //ghi file
+    void write(String filename);
+    //thêm
+    void them();
+    //xóa
+    void xoa();
+    //sửa
+    void sua(String ma);
+    //tìm theo mã -> index
+    int timTheoMa(String ma);
+    //in ds
+    void in();
+
+}
